7c24cb91a48b62d5fc2cc4839f870f372bee0a3b
Co <62308340+Cohen-Koen@users.noreply.github.com>
2022-09-02 12:01:29 -0500

    Update SSFT.ino

--- SSFT.ino
+++ SSFT.ino
@@ -3,15 +3,15 @@
 int value = 0; // variable to store the sensor value
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(23, OUTPUT);
+  Serial.begin(9600); // SERIAL OUT init
+  pinMode(23, OUTPUT); // PIN 23 is OUTPUT
 }
 
 void loop() {
                // wait 10 milliseconds
   value = analogRead(SIGNAL_PIN); // read the analog value from sensor
-  if(value > 120) {
-    digitalWrite(23,HIGH);
+  if(value > 120) { 
+    digitalWrite(23,HIGH); //23 5V
     
     }else{digitalWrite(23,LOW);}
   Serial.print("Sensor value: ");
